436b3ffec9fae11fc5d442d4180798c72b2a4347
yahel12 <137532984+yahel12@users.noreply.github.com>
2026-04-30 21:32:03 +0100

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -84,7 +84,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/SeriesAchievers')
+                    InlineKeyboardButton('🍁 ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/PSADOCX)
                 ]
             ]
                 )
