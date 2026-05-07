a7d6573722154e4b7af431a0080f78212a1a98a1
yahel12 <137532984+yahel12@users.noreply.github.com>
2026-05-07 05:15:01 +0100

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -16,7 +16,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 [
                     [
                     InlineKeyboardButton("⚡️ ᴄʟᴏsᴇ", callback_data = "close"),
-                    InlineKeyboardButton('🍁 ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/ZNRYX)
+                    InlineKeyboardButton('🍁 ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/ZNRYX')
                     ]
                 ]
             )
